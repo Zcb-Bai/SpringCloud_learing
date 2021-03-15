c7c2f1ae0934dc4b08ca81ee6cf88bb76d000f8f
Zcb-Bai <595452248@qq.com>
2021-03-15 15:34:42 +0800

    EurekaServer

---
+++ 01.eureka-server/src/main/java/com/spcd/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.spcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 启动类.
+ *
+ * @author zcb 2021/3/15
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
