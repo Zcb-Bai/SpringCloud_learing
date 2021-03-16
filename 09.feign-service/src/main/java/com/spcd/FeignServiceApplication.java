0c0411a3bd54648dbe40b7b52b4f2e9aae7e17f1
Zcb-Bai <595452248@qq.com>
2021-03-16 17:12:47 +0800

    feign服务调用测试服务

---
+++ 09.feign-service/src/main/java/com/spcd/FeignServiceApplication.java
@@ -0,0 +1,22 @@
+package com.spcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 启动类.
+ *
+ * @author zcb 2021/3/16
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FeignServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignServiceApplication.class, args);
+    }
+}
+
