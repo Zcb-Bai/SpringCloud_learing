20c8a8b3a0d0914255f9d6c71d4dbe7ce1c40b79
Zcb-Bai <595452248@qq.com>
2021-03-17 08:48:12 +0800

    zuul作为网关的测试服务

---
+++ 10.zuul-proxy/src/main/java/com/spcd/ZuulProxyApplication.java
@@ -0,0 +1,21 @@
+package com.spcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 启动类.
+ *
+ * @author zcb 2021/3/16
+ */
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZuulProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulProxyApplication.class, args);
+    }
+}
