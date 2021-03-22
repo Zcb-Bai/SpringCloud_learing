8351cf52e96fad1c4205662a5685e5a1d0c2b8cd
Zcb-Bai <595452248@qq.com>
2021-03-22 13:41:00 +0800

    配置中心和配置获取

---
+++ 11.config-server/src/main/java/com/spcd/ConfigServerApplication.java
@@ -0,0 +1,22 @@
+package com.spcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 启动类.
+ *
+ * @author zcb 2021/3/18
+ */
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
---
+++ 12.config-client/src/main/java/com/spcd/ConfigClientApplication.java
@@ -0,0 +1,18 @@
+package com.spcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zcb
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
