208803185534844e2c3c271a5ecb8b865f80cd49
Zcb-Bai <595452248@qq.com>
2021-03-23 15:42:20 +0800

    consul服务治理和负载均衡

---
+++ 13.consul-user-service/src/main/java/com/spcd/ConsulUserServiceApplication.java
@@ -0,0 +1,18 @@
+package com.spcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zcb
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsulUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulUserServiceApplication.class, args);
+    }
+
+}
